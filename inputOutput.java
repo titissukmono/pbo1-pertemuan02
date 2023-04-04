e8d532446e0b78d1729e079c08187880fefb4954
titis_sukmono <titisbaen91@gmail.com>
2023-04-04 13:50:13 -0700

    Buat import string ke dalam variabel

--- inputOutput.java
+++ inputOutput.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class inputOutput{
 public static void main(String [] args){
 Scanner scanner = new Scanner(System.in);
+string nama;
+System.out.print("Masukan Nama anda : ");
+nama =scanner.nextLine();
 
 }
 }
