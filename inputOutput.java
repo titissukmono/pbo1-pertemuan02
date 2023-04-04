0ac3500d6962245d5c657779099801a79d76fe61
titis_sukmono <titisbaen91@gmail.com>
2023-04-04 13:53:04 -0700

    Update inputOutput.java

--- inputOutput.java
+++ inputOutput.java
@@ -5,7 +5,7 @@ public static void main(String [] args){
 Scanner scanner = new Scanner(System.in);
 string nama;
 System.out.print("Masukan Nama anda : ");
-nama =scanner.nextLine();
+nama = scanner.nextLine();
 System.out.println("Hai,"+nama);
 
 }
