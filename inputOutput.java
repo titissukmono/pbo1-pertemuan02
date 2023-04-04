ce6fd7c27831f71b0d5a10f5d652d6ac74ec5616
titis_sukmono <titisbaen91@gmail.com>
2023-04-04 14:03:34 -0700

    Compile and Run

--- inputOutput.java
+++ inputOutput.java
@@ -4,9 +4,23 @@ public class inputOutput{
 public static void main(String [] args){
 Scanner scanner = new Scanner(System.in);
 String nama;
+int sks;
+double ipk;
+
 System.out.print("Masukan Nama anda : ");
 nama = scanner.nextLine();
 System.out.println("Hai,"+nama);
+
+
+System.out.print("Masukan Jumlah sks anda : ");
+sks = scanner.nextInt();
+
+
+System.out.print("Masukan IPK Sementara : ");
+ipk = scanner.nextDouble();
+
+double jumlah =ipk*sks;
+System.out.print("Total Nilai ="+jumlah);
 
 }
 }
